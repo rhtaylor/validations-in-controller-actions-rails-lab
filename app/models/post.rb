b0a91c4ae91e6fe5f662e5efb0127b351e92bef0
rhtaylor <randyisbusy@gmail.com>
2019-12-14 09:16:43 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true 
+    validates :content, presence:true, length: { minimum:  100}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), 
+    message: "%{value} is not a valid category" }
 end
